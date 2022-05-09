04b0735b48671507186642ea5cc3ca1323328cb5
lylelove <lylelove@163.com>
2022-05-09 21:02:59 +0800

    1

---
+++ web/g1.js
@@ -0,0 +1,8 @@
+document.write('<link href="https://cdn.jsdelivr.net/gh/lylelove/GuoNanSimulator/css/app.47094c85.css" rel="preload" as="style">');
+document.write('<link href="https://cdn.jsdelivr.net/gh/lylelove/GuoNanSimulator/css/chunk-vendors.a16c4353.css" rel="preload" as="style">');
+document.write('<link href="https://cdn.jsdelivr.net/gh/lylelove/GuoNanSimulator/js/app.e1175a50.js" rel="preload" as="script">');
+document.write('<link href="https://cdn.jsdelivr.net/gh/lylelove/GuoNanSimulator/js/chunk-vendors.68fe4d71.js" rel="preload" as="script">');
+document.write('<link href="https://cdn.jsdelivr.net/gh/lylelove/GuoNanSimulator/css/chunk-vendors.a16c4353.css" rel="stylesheet">');
+document.write('<link href="https://cdn.jsdelivr.net/gh/lylelove/GuoNanSimulator/css/app.47094c85.css" rel="stylesheet">');
+document.write('<script src="https://cdn.jsdelivr.net/gh/lylelove/GuoNanSimulator/js/chunk-vendors.68fe4d71.js"></script>');
+document.write('<script src="https://cdn.jsdelivr.net/gh/lylelove/GuoNanSimulator/js/app.e1175a50.js"></script>');
