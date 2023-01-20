7256214e4a8b3ff18c18bc63dbdc66fbc8f2458d
lyle <lylelove@163.com>
2023-01-20 09:08:09 +0800

    23.1.20

---
+++ youhou001.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.tampermonkey.net/index.php
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        GM_log
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    GM_log('Hello world!');
+    // Your code here...
+})();
